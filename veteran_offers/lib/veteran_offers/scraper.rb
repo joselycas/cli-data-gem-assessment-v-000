befb1dd6ac609bd72d61beb12528b90ee2cdeff1
Jos Castro <joselycas@gmail.com>
2018-08-01 19:14:39 +0000

    automatically backed up by learn

--- veteran_offers/lib/veteran_offers/scraper.rb
+++ veteran_offers/lib/veteran_offers/scraper.rb
@@ -2,7 +2,7 @@ class VeteranOffers::Scraper
 
   def self.scrape_vawebsite
     doc = Nokogiri::HTML(open("https://members.veteransadvantage.com/deals/travel-lifestyle"))
-    doc.css(".discount").collect do |discount|
+    doc.css(".discount").each do |discount|
       offer = VeteranOffers::Offer.new
       offer.details = discount.css(".description p").text.gsub("\n", "")
       offer.discount_type = discount.css(".discount-type").text.gsub("\n", "")
